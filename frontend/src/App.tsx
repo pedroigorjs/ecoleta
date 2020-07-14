9b16d0fea1246ce10cc685c72690b43f47e5dec1
Pedro Sampaio <pedroigorjs@gmail.com>
2020-07-14 07:47:43 -0300

    Create Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import Routes from './routes';
 import GlobalStyles from './styles/global';
 
 const App: React.FC = () => {
   return (
     <>
+      <Routes />
       <GlobalStyles />
     </>
   );
