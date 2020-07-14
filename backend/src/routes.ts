51f40644fb856f7fbedf72b99ddf251df356b3c6
Pedro Sampaio <pedroigorjs@gmail.com>
2020-07-14 07:37:01 -0300

    Create routes

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,7 +1,14 @@
 import { Router } from 'express';
 
+import ItemsController from './controllers/ItemsController';
+import PointsController from './controllers/PointsController';
+
 const routes = Router();
 
-routes.get('/', (request, response) => response.json({ ok: true }));
+routes.get('/items', ItemsController.index);
+
+routes.post('/points', PointsController.create);
+routes.get('/points', PointsController.index);
+routes.get('/points/:id', PointsController.show);
 
 export default routes;
