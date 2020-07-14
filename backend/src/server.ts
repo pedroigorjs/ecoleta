40118384c90c7d03c86d0dac55a7c744d416cf4b
Pedro Sampaio <pedroigorjs@gmail.com>
2020-07-14 07:37:21 -0300

    Create uploads static folder

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,7 @@
 import 'dotenv/config';
 import express from 'express';
+import { resolve } from 'path';
+import cors from 'cors';
 
 import routes from './routes';
 
@@ -7,8 +9,10 @@ const app = express();
 
 const { HOST = 'localhost', PORT = 3333 } = process.env;
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use('/uploads', express.static(resolve(__dirname, '..', 'uploads')));
 
 app.listen(PORT, () => {
   console.log(`🚀 Server is running at http://${HOST}:${PORT}`);
